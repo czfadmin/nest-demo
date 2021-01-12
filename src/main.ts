8b49967e7d6a6d465fb71a490967d4a4738413e8
czfadmin <www.2849007069@outlook.com>
2021-01-12 16:31:05 +0800

    启用cors,同时启用csurf,防止跨站点请求伪造

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import * as helmet from 'helmet';
-
+import * as csurf from 'csurf';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		cors: true,
+	});
 	app.useGlobalPipes(new ValidationPipe({
 		disableErrorMessages: true,
 		transform: true,
 	}));
 	app.use(helmet())
+	app.use(csurf())
 	await app.listen(3000);
 }
 bootstrap();
