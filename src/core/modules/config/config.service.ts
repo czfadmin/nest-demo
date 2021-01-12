0c85c64c5b637ab63d17584fe49aeb7fdb8a91a9
czfadmin <www.2849007069@outlook.com>
2021-01-12 15:57:00 +0800

    配置验证

--- src/core/modules/config/config.service.ts
+++ src/core/modules/config/config.service.ts
@@ -3,15 +3,38 @@ import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as Joi from '@hapi/joi';
 
+
+
+export type EnvConfig = Record<string, string>;
+
 @Injectable()
 export class ConfigService {
-	private readonly envConfig: Record<string, string>;
+	private readonly envConfig: EnvConfig;
 	constructor(filePath: string) {
-		this.envConfig = dotenv.parse(fs.readFileSync(filePath))
+		const config = dotenv.parse(fs.readFileSync(filePath));
+		this.envConfig = this.validateInput(config);
 	}
 
 	get(key: string): string {
 		return this.envConfig[key];
+	}
+
+
+	private validateInput(envConfig: EnvConfig): EnvConfig {
+		const envVarsSchema: Joi.ObjectSchema = Joi.object({
+			NODE_ENV: Joi.string().valid('development', 'production', 'test', 'provision').default('development'),
+			PORT: Joi.number().default(3000),
+			API_AUTH_ENABLED: Joi.boolean().required()
+		});
+		const { error, value: validedEnvConfig } = envVarsSchema.validate(envConfig,);
+		if (error) {
+			throw new Error(`Config validate error: ${error.message}`);
+		}
+		return validedEnvConfig;
+	}
+
+	get isApiAuthEnabled(): boolean {
+		return Boolean(this.envConfig.API_AUTH_ENABLED)
 	}
 
 
