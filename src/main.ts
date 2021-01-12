4bbc540835d9dc66383c721fa6d0cf6caac1ebd9
czfadmin <www.2849007069@outlook.com>
2021-01-12 16:33:51 +0800

    添加限速功能

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestFactory } from '@nestjs/core';
 
 import * as helmet from 'helmet';
 import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
+
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,8 +16,12 @@ async function bootstrap() {
 		disableErrorMessages: true,
 		transform: true,
 	}));
-	app.use(helmet())
-	app.use(csurf())
+	app.use(helmet());
+	app.use(csurf());
+	app.use(rateLimit({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // limit each IP to 100 requests per windowMs
+	}));
 	await app.listen(3000);
 }
 bootstrap();
