fa87f46278fc5b4d3e90530b75d66920c4631090
czfadmin <www.2849007069@outlook.com>
2021-01-13 09:02:38 +0800

    添加swagger

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import * as rateLimit from 'express-rate-limit';
 
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
@@ -22,6 +23,17 @@ async function bootstrap() {
 		windowMs: 15 * 60 * 1000, // 15 minutes
 		max: 100, // limit each IP to 100 requests per windowMs
 	}));
+
+	// swagger
+	const options = new DocumentBuilder()
+		.setTitle('Cats example')
+		.setDescription('The cats API description')
+		.setVersion('1.0')
+		.addTag('cats')
+		.build();
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup('api', app, document);
+
 	await app.listen(3000);
 }
 bootstrap();
